ddee5861922ba6535fcf8cbf35971385c47bf35b
meryemgulbudakk <meryem.gulbudakk@gmail.com>
2024-12-22 14:02:49 +0300

    Add KafkaLogManagementApplication class with Spring Boot setup

---
+++ src/main/java/com/kafka/logmanagement/KafkaLogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.kafka.logmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaLogManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaLogManagementApplication.class, args);
+	}
+
+}
